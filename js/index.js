ff435d94293e428ddfac1926d62b0bedcf816708
JohnieZhang <1046944133@qq.com>
2018-10-14 16:10:09 +0800

    add loading img

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
 window.onload=function(){
+    var container=document.getElementById('container');
+    var preItem=document.querySelector('.preItem');
+    var preloader=document.querySelector('.preloader');
     var spans=document.querySelectorAll(".item div span");
     var works=document.querySelectorAll(".works");
-    
+    var documentH=document.documentElement.clientHeight || document.body.clientHeight;
+    console.log(documentH)
+    container.style.height=documentH+'px';
+    preItem.style.height=documentH+'px';
+    setTimeout(function(){
+        preItem.style.display="none";
+        container.style.display="block";
+        preItem.style.opacity=0;
+    },2000)
     for(let i=0;i<spans.length;i++){
         spans[i].index=i;
         spans[i].onclick=function(){
